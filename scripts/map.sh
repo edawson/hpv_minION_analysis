4c99726ebd86c4b41bf510c35dcf9a14f1f2d683
Eric T Dawson <eric.t.dawson@gmail.com>
2016-03-29 20:21:47 +0100

    Refactored and added updated Makefile

---
+++ scripts/map.sh
@@ -0,0 +1,7 @@
+vg=
+kmer_size=
+gcsa=
+xg=
+fastq=
+o_base=
+${vg} map -k ${kmer_size} -g ${gcsa} -x ${xg} -f ${fastq} > ${o_base}.gam
---
+++ scripts/viz.sh
@@ -0,0 +1,8 @@
+node_type="oval"
+in_dot=$1
+
+## remove labels over 1bp in length
+
+## change to round/elliptical nodes
+
+## color snps
