099383e3cb94cbd1d93ecfc24e88e6db75282c5e
Eric T Dawson <eric.t.dawson@gmail.com>
2016-03-31 13:28:48 +0100

    Added shell script for simulating reads. Pushing to github as a skeleton

---
+++ scripts/sim_ref_reads.sh
@@ -0,0 +1,8 @@
+## Simulates reads from three different HPV
+## lineage reference genomes. This simulation is
+## run for a range of error rates.
+
+## this data is then used as input to vg
+## vectorize and clustering algorithms to
+## demonstrate VG's ability to classify reads
+## by genomic feature coverage.
