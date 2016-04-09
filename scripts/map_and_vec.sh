d029a32afc18f8b4edd2670754cbc90e8a1d7c10
Eric T Dawson <eric.t.dawson@gmail.com>
2016-04-09 10:09:03 +0100

    Adding script to map and vectorize reads

---
+++ scripts/map_and_vec.sh
@@ -0,0 +1,33 @@
+vg=~/sandbox/vg/bin/vg
+
+hpv_sixteen=../hpv_16.fa
+
+node_len=50
+
+num_reads=1000
+read_len=7500
+
+bandwidth=20000
+
+function construct () {
+    ${vg} construct -m ${node_len} -r $1 > `basename $1 .fa`.vg;
+    echo "`basename $1 .fa`.vg";
+}
+
+${vg} construct -r ${hpv_sixteen} -m ${node_len} > hpv_16.vg
+${vg} index -x hpv_16.xg -k 16 -g hpv_16.gcsa hpv_16.vg
+
+
+##hpv_16.1000reads.error0.0.length7500.indel0.0.txt
+
+for error_rate in 0.0 0.01 0.05 0.15 0.25
+do
+    cat hpv_*error${error_rate}.*indel0.0.txt > tmpError.txt
+    ${vg} map -t 4 -GX -B ${bandwidth} -r tmpError.txt -x hpv_16.xg -g hpv_16.gcsa | ${vg} vectorize -x hpv_16.xg - > hpv_16_Aligned.error${error_rate}.bandwidth${bandwidth}vecs.txt
+done
+
+
+for indel_rate in 0.0000 0.01 0.05 0.15 0.25
+do
+     ${vg} map -t 4 -GX -B ${bandwidth} -r hpv_*error0.0.*indel${indel_rate}.txt -x hpv_16.xg -g hpv_16.gcsa | ${vg} vectorize -x hpv_16.xg - > hpv_16_Aligned.indel${indel_rate}.vecs.txt
+done
