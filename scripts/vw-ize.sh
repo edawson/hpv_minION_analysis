0e26e98a2a0fe76f102cac5a4831e6cae9e5affa
Eric T Dawson <eric.t.dawson@gmail.com>
2016-04-13 14:22:56 +0100

    Added SH script for transformation to vw format

---
+++ scripts/vw-ize.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+
+control_c(){
+    exit $?
+}
+
+
+
+## currently just greps the last digits after hpv. pretty crude.
+
+name_to_cat (){
+    ret=$(echo "$1" | cut -f 2 -d "_")
+    echo $ret
+}
+
+gen_features (){
+    ret=""
+    count=0
+    for i in $1
+    do
+        count=$(($count + 1))
+        ret+="${count}:$(($i - 1)) "
+    done
+    echo ${ret}
+}
+
+
+count=0
+trap control_c INT SIGTERM SIGINT
+while read line
+do
+    label=$(echo $line | cut -f 1 | grep -o "hpv_[0-9]\{1,10\}")
+    category=$(name_to_cat $label)
+    features=`gen_features "$(echo $line | cut -f2- -d" ")"`
+    echo "${category} 1.0 $label| ${features}"
+done
