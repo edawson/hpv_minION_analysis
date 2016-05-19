3888391f8ee442bc9d219f8b939c0fb6bac573ae
Eric T Dawson <eric.t.dawson@gmail.com>
2016-05-19 09:05:06 +0100

    Refactoring

--- 3_HPV_PanGenome/config.sh
+++
@@ -1,6 +0,0 @@
-
-CWD=`pwd`
-for i in scripts/*
-do
-    sed -e 's/mash=.*$/mash=${CWD}/Mash/mash/' scripts/$i
-done
--- 3_HPV_PanGenome/scripts/viz.sh
+++
@@ -1,3 +0,0 @@
-vg=../vg/bin/vg
-
-${vg} view -dSp random.vg | neato -Tpng -o ~/Dropbox/x_$1.png
